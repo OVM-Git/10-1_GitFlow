1d145179d2cde8004d63e76045d22ae146326bc2
Vyacheslav Ovchinnikov <slava-om@yandex.ru>
2025-01-27 20:35:06 +0300

    funcshion clear names

---
+++ src/main.py
@@ -0,0 +1,20 @@
+def clear_names(file_name: str) -> list:
+    """ Функция для очистки имён от лишних символов """
+    new_names_lict = list()
+    with open('data/' + file_name) as names_file:
+        names_list = names_file.read().split()
+        for name_item in names_list:
+            new_name = ''
+            for symbol in name_item:
+                if symbol.isalpha():
+                    new_name += symbol
+            if new_name.isalpha():
+                new_names_lict.append(new_name)
+    return new_names_lict
+
+
+if __name__ == '__main__':
+    cleared_name = clear_names('names.txt')
+
+    for i in cleared_name:
+        print(i)
